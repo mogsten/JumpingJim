87487038e5d0e678647ced6dfbd18a86b3c73652
Alexander Mogavero <mogsten@outlook.com>
2015-12-08 12:23:25 +1030

    Commit 1 - Basic Template of Application

---
+++ Jumping JimTests/Jumping_JimTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jumping_JimTests.swift
+//  Jumping JimTests
+//
+//  Created by Alexander Mogavero on 8/12/2015.
+//  Copyright © 2015 Alexander Mogavero. All rights reserved.
+//
+
+import XCTest
+@testable import Jumping_Jim
+
+class Jumping_JimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
